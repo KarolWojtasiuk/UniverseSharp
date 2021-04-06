84e1b64905fe5640755233b89815296f7345b3cd
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2021-04-06 16:34:13 +0200

    feat: Add Universe helper class

---
+++ UniverseSharp/Universe.cs
@@ -0,0 +1,15 @@
+namespace UniverseSharp
+{
+    /// <summary>
+    /// Helper class for laws of physics.
+    /// </summary>
+    public static class Universe
+    {
+        /// <summary>
+        /// Represents gravitational constant.
+        /// <para/>
+        /// Unit: m³kg⁻¹s⁻²
+        /// </summary>
+        public const decimal GravitationalConstant = 6.67408e-11m;
+    }
+}
